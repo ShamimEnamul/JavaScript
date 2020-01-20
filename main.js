919fa47d1408494e4d897b4549298a222702d678
Md. Wahidul Azam <wahidul@sdslbd.com>
2020-01-20 18:25:47 +0600

    from office -master

--- main.js
+++ main.js
@@ -33,10 +33,36 @@ class UI{
 
    static addBookToList(book)
    {
-        const lsit = document.getElementById('book-list');
+        const list = document.getElementById('book-list');
         const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class = "btn btn-danger btn-sm delete">X</a></td>
+        `;
+        list.appendChild(row);
+
    }
 }
-//event add book
+
 //event display book
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+//event add book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const isbn = document.querySelector('#isbn').value;
+
+//initialize book
+const b = new Book(title,author,isbn);
+
+
+});
+
+
+
+
 //event delete book
